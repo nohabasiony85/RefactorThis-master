ef08cb52591318785867291e32b58280d2e16926
Noha Basiony <nohabasiony@outlook.com>
2024-03-20 00:31:47 +1100

    update UpdateProductCommandHandler

--- RefactorThis.Domain/Products/UpdateProductCommand/UpdateProductCommandHandler.cs
+++ RefactorThis.Domain/Products/UpdateProductCommand/UpdateProductCommandHandler.cs
@@ -2,9 +2,9 @@ using RefactorThis.Domain.Abstractions;
 
 namespace RefactorThis.Domain.Products.UpdateProductCommand;
 
-public class UpdateProductCommandHandler : ICommandHandler<CreateProductCommand.CreateProductCommand, Guid>
+public class UpdateProductCommandHandler : ICommandHandler<UpdateProductCommand, Guid>
 {
-    public Task<Guid> Handle(CreateProductCommand.CreateProductCommand request, CancellationToken cancellationToken)
+    public Task<Guid> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
