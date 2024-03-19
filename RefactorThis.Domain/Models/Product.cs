659feb6d1e3b3aa4b1c5a8ba81441569c1285e04
Noha Basiony <nohabasiony@outlook.com>
2024-03-20 00:09:57 +1100

    update Product & Products

--- RefactorThis.Domain/Models/Product.cs
+++ RefactorThis.Domain/Models/Product.cs
@@ -1,72 +1,69 @@
-﻿using System.Text.Json.Serialization;
-using Microsoft.Data.Sqlite;
-
-namespace RefactorThis.Domain.Models {
+﻿namespace RefactorThis.Domain.Models {
     public class Product
     {
         public Guid Id { get; set; }
 
-        public string Name { get; set; }
+        public required string Name { get; init; }
 
-        public string Description { get; set; }
+        public required string Description { get; init; }
 
-        public decimal Price { get; set; }
+        public decimal Price { get; init; }
 
-        public decimal DeliveryPrice { get; set; }
+        public decimal DeliveryPrice { get; init; }
 
-        [JsonIgnore] public bool IsNew { get; }
+        // [JsonIgnore] public bool IsNew { get; }
 
-        public Product()
-        {
-            Id = Guid.NewGuid();
-            IsNew = true;
-        }
+        // public Product()
+        // {
+        //     Id = Guid.NewGuid();
+        //     IsNew = true;
+        // }
 
-        public Product(Guid id)
-        {
-            IsNew = true;
-            var conn = Helpers.NewConnection<SqliteConnection>();
-            conn.Open();
-            var cmd = conn.CreateCommand();
-            cmd.CommandText = $"select * from Products where id = '{id}' collate nocase";
+        // public Product(Guid id)
+        // {
+        //     IsNew = true;
+        //     var conn = Helpers.NewConnection<SqliteConnection>();
+        //     conn.Open();
+        //     var cmd = conn.CreateCommand();
+        //     cmd.CommandText = $"select * from Products where id = '{id}' collate nocase";
+        //
+        //     var rdr = cmd.ExecuteReader();
+        //     if (!rdr.Read())
+        //         return;
+        //
+        //     IsNew = false;
+        //     Id = Guid.Parse(rdr["Id"].ToString());
+        //     Name = rdr["Name"].ToString();
+        //     Description = (DBNull.Value == rdr["Description"]) ? null : rdr["Description"].ToString();
+        //     Price = decimal.Parse(rdr["Price"].ToString());
+        //     DeliveryPrice = decimal.Parse(rdr["DeliveryPrice"].ToString());
+        // }
 
-            var rdr = cmd.ExecuteReader();
-            if (!rdr.Read())
-                return;
+        // public void Save()
+        // {
+        //     var conn = Helpers.NewConnection<SqliteConnection>();
+        //     conn.Open();
+        //     var cmd = conn.CreateCommand();
+        //
+        //     cmd.CommandText = IsNew
+        //         ? $"insert into Products where (id, name, desc, price, deliveryprice) values ('{Id}', '{Name}', '{Description}', {Price}, {DeliveryPrice})"
+        //         : $"update Products set name = '{Name}', description = '{Description}', price = {Price}, deliveryprice = {DeliveryPrice} where id = '{Id}' collate nocase";
+        //
+        //     conn.Open();
+        //     cmd.ExecuteNonQuery();
+        // }
 
-            IsNew = false;
-            Id = Guid.Parse(rdr["Id"].ToString());
-            Name = rdr["Name"].ToString();
-            Description = (DBNull.Value == rdr["Description"]) ? null : rdr["Description"].ToString();
-            Price = decimal.Parse(rdr["Price"].ToString());
-            DeliveryPrice = decimal.Parse(rdr["DeliveryPrice"].ToString());
-        }
-
-        public void Save()
-        {
-            var conn = Helpers.NewConnection<SqliteConnection>();
-            conn.Open();
-            var cmd = conn.CreateCommand();
-
-            cmd.CommandText = IsNew
-                ? $"insert into Products where (id, name, desc, price, deliveryprice) values ('{Id}', '{Name}', '{Description}', {Price}, {DeliveryPrice})"
-                : $"update Products set name = '{Name}', description = '{Description}', price = {Price}, deliveryprice = {DeliveryPrice} where id = '{Id}' collate nocase";
-
-            conn.Open();
-            cmd.ExecuteNonQuery();
-        }
-
-        public void Delete()
-        {
-            foreach (var option in new ProductOptions(Id).Items)
-                option.Delete();
-
-            var conn = Helpers.NewConnection<SqliteConnection>();
-            conn.Open();
-            var cmd = conn.CreateCommand();
-
-            cmd.CommandText = $"delete from Products where id = '{Id}' collate nocase";
-            cmd.ExecuteNonQuery();
-        }
+        // public void Delete()
+        // {
+        //     foreach (var option in new ProductOptions(Id).Items)
+        //         option.Delete();
+        //
+        //     var conn = Helpers.NewConnection<SqliteConnection>();
+        //     conn.Open();
+        //     var cmd = conn.CreateCommand();
+        //
+        //     cmd.CommandText = $"delete from Products where id = '{Id}' collate nocase";
+        //     cmd.ExecuteNonQuery();
+        // }
     }
 }
