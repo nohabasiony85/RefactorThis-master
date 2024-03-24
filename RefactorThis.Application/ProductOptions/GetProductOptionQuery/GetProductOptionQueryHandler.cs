5f0c073b1e94f4522947cf67bdc7300f81b1773e
Noha Basiony <nohabasiony@outlook.com>
2024-03-25 02:12:06 +1100

    GetProductOptionQueryHandler

--- RefactorThis.Application/ProductOptions/GetProductOptionQuery/GetProductOptionQueryHandler.cs
+++ RefactorThis.Application/ProductOptions/GetProductOptionQuery/GetProductOptionQueryHandler.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.Sqlite;
 using RefactorThis.Application.Abstractions;
 using RefactorThis.Application.Abstractions.Data;
 using RefactorThis.Domain.Entities;
@@ -12,16 +13,17 @@ public class GetProductOptionQueryHandler(ISqlDataConnectionFactory sqlDataConne
         var connection = sqlDataConnectionFactory.CreateConnection();
 
         var command = connection.CreateCommand();
-        command.CommandText = $"select * from productoptions where id = '{request.ProductId}' collate nocase";
-
+        command.CommandText = "SELECT * FROM productoptions WHERE id = @Id";
+            
+        command.Parameters.Add(new SqliteParameter("@Id", request.ProductId));
         var reader = command.ExecuteReader();
 
         while (reader.Read())
         {
-            productOption.Id = (Guid)reader["Id"];
-            productOption.ProductId = (Guid)reader["ProductId"];
-            productOption.Name = (string)reader["Name"];
-            productOption.Description = (string)reader["Description"];
+            productOption.Id = reader.GetGuid(reader.GetOrdinal("Id"));
+            productOption.ProductId = reader.GetGuid(reader.GetOrdinal("ProductId"));
+            productOption.Name = reader.GetString(reader.GetOrdinal("Name"));
+            productOption.Description = reader.GetString(reader.GetOrdinal("Description"));
         }
 
         return Task.FromResult(productOption);
