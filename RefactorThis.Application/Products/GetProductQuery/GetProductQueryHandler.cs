93dfc7996f24d14436feb162ff87043c2d1ab677
amrreda <amrreda@outlook.com>
2024-03-25 00:20:34 +1100

    fix sql in ProductQueryHandler

--- RefactorThis.Application/Products/GetProductQuery/GetProductQueryHandler.cs
+++ RefactorThis.Application/Products/GetProductQuery/GetProductQueryHandler.cs
@@ -1,3 +1,5 @@
+using System.Data;
+using Microsoft.Data.Sqlite;
 using RefactorThis.Application.Abstractions;
 using RefactorThis.Application.Abstractions.Data;
 using RefactorThis.Domain.Entities;
@@ -9,20 +11,23 @@ public class GetProductQueryHandler(ISqlDataConnectionFactory sqlDataConnectionF
     public Task<Product> Handle(GetProductQuery request, CancellationToken cancellationToken)
     {    
         var product = new Product();
-        var connection = sqlDataConnectionFactory.CreateConnection();
-
-        var command = connection.CreateCommand();
-        command.CommandText = $"select * from products where id = '{request.Id}'";
-
-        var reader = command.ExecuteReader();
-
-        while (reader.Read())
+        using (var connection = sqlDataConnectionFactory.CreateConnection())
         {
-            product.Id = (Guid)reader["Id"];
-            product.Name = (string)reader["Name"];
-            product.Description = (string)reader["Description"];
-            product.Price = (decimal)reader["Price"];
-            product.DeliveryPrice = (decimal)reader["DeliveryPrice"];
+            var command = connection.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT * FROM products WHERE id = @Id";
+            command.Parameters.Add(new SqliteParameter("@Id", SqlDbType.UniqueIdentifier) { Value = request.Id });
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                product.Id = reader.GetGuid(reader.GetOrdinal("Id"));
+                product.Name = reader.GetString(reader.GetOrdinal("Name"));
+                product.Description = reader.GetString(reader.GetOrdinal("Description"));
+                product.Price = reader.GetDecimal(reader.GetOrdinal("Price"));
+                product.DeliveryPrice = reader.GetDecimal(reader.GetOrdinal("DeliveryPrice"));
+            }
         }
 
         return Task.FromResult(product);
